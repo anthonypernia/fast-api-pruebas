57c7a6931fef08c438f3da518df993e17566b2a2
anthony pernia <anthonyperniah@gmail.com>
2022-01-01 20:16:59 +0000

    validation_query

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 #Python
 from typing import Optional
-
 #Pydantic
 from pydantic import BaseModel
 
 #FastAPI
 from fastapi import FastAPI
-from fastapi import Body
+from fastapi import Body, Query
 
 app = FastAPI()
 
@@ -29,3 +28,9 @@ def home():
 @app.post('/person/new')
 def create_person(person: Person = Body(...)): #El triple punto indica que es oblicatorio el body
     return person
+
+#Validation query paramns
+@app.get("/person/detail/" )
+def show_person(name: Optional[str] = Query(default=None, min_length=1, max_length=50), ##Asi cuando es opcional, y le colocas un valor por defecto
+                age: str = Query(...)): ##Se coloca asi cuando quieres que sea obligatorio, los tres puntos
+    return {name, age}
