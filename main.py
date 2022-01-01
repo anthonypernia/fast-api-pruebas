f620584d002e357577d4008ddc59257db9b1cfed
anthony pernia <anthonyperniah@gmail.com>
2022-01-01 20:30:38 +0000

    validation_path

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 
 #FastAPI
 from fastapi import FastAPI
-from fastapi import Body, Query
+from fastapi import Body, Query, Path, Header
 
 app = FastAPI()
 
@@ -31,6 +31,28 @@ def create_person(person: Person = Body(...)): #El triple punto indica que es ob
 
 #Validation query paramns
 @app.get("/person/detail/" )
-def show_person(name: Optional[str] = Query(default=None, min_length=1, max_length=50), ##Asi cuando es opcional, y le colocas un valor por defecto
-                age: str = Query(...)): ##Se coloca asi cuando quieres que sea obligatorio, los tres puntos
+def show_person(name: Optional[str] = Query(
+                default=None, 
+                min_length=1, 
+                max_length=50,
+                title="person name",
+                description= "this is the person name, between 1 and 50 characters",
+                ), ##Asi cuando es opcional, y le colocas un valor por defecto
+                age: str = Query(
+                            ..., 
+                            title="Person age",
+                            description="this is the person age",
+                            )): ##Se coloca asi cuando quieres que sea obligatorio, los tres puntos
     return {name, age}
+
+
+##NOTA: se va a tomar este path, porque python lee de arriba hacia abajo, por ende se va a quedar con el ultimo path que se haya definido
+#validation path parameters
+@app.get("/person/detail/{person_id}")
+def show_person(person_id: int = Path(
+                                    ..., 
+                                    gt=0,
+                                    title="person id",
+                                    description="this is the person id, greater than 0",
+                                    )): ##Obligatorio
+    return {"person_id": person_id}
